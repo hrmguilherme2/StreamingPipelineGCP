55bf7b8ca83a27ce31fb00f20e25ac778ab9b0fc
hrmguilherme2 <39834493+hrmguilherme2@users.noreply.github.com>
2019-10-02 19:28:39 -0300

    Update Beam.scala

--- src/main/scala/com/guilherme/Beam.scala
+++ src/main/scala/com/guilherme/Beam.scala
@@ -1,12 +1,17 @@
 package com.guilherme
 
 import java.io.FileInputStream
+import java.io.FileInputStream
 
 import com.typesafe.scalalogging.LazyLogging
 import com.google.api.services.bigquery.model.{ TableReference, TableRow }
 import com.google.api.services.dataflow.DataflowScopes
 import com.google.auth.oauth2.ServiceAccountCredentials
+import com.google.gson.JsonParser
 import org.apache.beam.runners.dataflow.DataflowRunner
+import org.apache.beam.sdk.transforms.View
+import org.apache.beam.sdk.transforms.windowing._
+import org.apache.beam.sdk.values.PCollectionView
 //import org.apache.beam.runners.dataflow.DataflowRunner
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO
@@ -19,7 +24,6 @@ import org.apache.beam.runners.direct.DirectRunner
 //import org.apache.beam.sdk.coders.{ AvroCoder, DefaultCoder }
 import org.apache.beam.sdk.util.Transport
 import scala.util.{ Failure, Success, Try }
-
 object Beam extends App {
 
   /* credencial  gerada no gcp */
@@ -31,10 +35,11 @@ object Beam extends App {
   val dataset = "test_dataset"
   val tableName = "test"
   val subscription = "test-dataflow"
+  val sideInputSubscription = "test-dataflow-sideinput"
 
   /* config para o runner */
-  val runner = classOf[DataflowRunner]
-  //val runner = classOf[DirectRunner]
+  //val runner = classOf[DataflowRunner]
+  val runner = classOf[DirectRunner]
   val numWorkers = 1
   val zone = "us-west1-a"
   val workerMachineType = "n1-standard-1"
@@ -54,37 +59,51 @@ object Beam extends App {
 
   /* Pipeline le a mensagem do input e faz streaming direto no BigQuery  */
   val fullSubscriptionName = s"projects/$projectId/subscriptions/$subscription"
+  val fullSideInputSubscriptionName = s"projects/$projectId/subscriptions/$sideInputSubscription"
   val targetTable = new TableReference().setProjectId(projectId).setDatasetId(dataset).setTableId(tableName)
 
   /* Tentando converter Strings para TableRow */
-  class MyDoFn extends DoFn[String, TableRow] with LazyLogging {
+  class MyDoFn(sideView: PCollectionView[java.util.List[String]]) extends DoFn[String, TableRow] with LazyLogging {
     @ProcessElement
     def processElement(c: ProcessContext) {
+      val sideInput = c.sideInput(sideView).get(0)
       val inputString = c.element()
-      logger.info(s"Mensagem recebida: $inputString")
-      Try {
-        Transport.getJsonFactory.fromString(inputString, classOf[TableRow])
-      } match {
-        case Success(row) ⇒
-          logger.info(s"Convertido TableRow: $row")
-          c.output(row)
-        case Failure(ex) ⇒
-          logger.info(s"Erro: $inputString", ex)
+      if (sideInput == "ENABLED") {
+        Try {
+          Transport.getJsonFactory.fromString(inputString, classOf[TableRow])
+        } match {
+          case Success(row) ⇒
+            logger.info(s"Inserting to BiqQuery: $row")
+            c.output(row)
+          case Failure(ex) ⇒
+            logger.info(s"Unable to parse message: $inputString", ex)
+        }
+      } else {
+        logger.info(s"Ignoring input messages, sideInput=$sideInput")
       }
     }
   }
 
   /* construção de pipeline para ler Strings do PubsubIO, convertê-las em TableRows e gravar na tabela BQ, não validando o esquema da tabela */
   val p = Pipeline.create(options)
+
+  /* lendo o topico de controle */
+  val sideView = p.apply("read-pubsub-side", PubsubIO.readStrings().fromSubscription(fullSideInputSubscriptionName))
+    .apply("global_side_input", Window.into[String](new GlobalWindows())
+      .triggering(Repeatedly.forever(AfterPane.elementCountAtLeast(1)))
+      .discardingFiredPanes())
+    .apply("side_view", View.asList())
+
+  /* final pipeline */
   p.apply("read-pubsub", PubsubIO.readStrings().fromSubscription(fullSubscriptionName))
-    .apply("process", ParDo.of(new MyDoFn))
+    .apply("process", ParDo.of(new MyDoFn(sideView)).withSideInputs(sideView))
     .apply("write-bq", BigQueryIO
       .writeTableRows()
       .to(targetTable)
       .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
       .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_NEVER))
 
-  /* inicia pipeline */
+  /* start pipeline */
   p.run()
 
 }
